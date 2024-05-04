474b0902b46da1f375f997e616a9971ea5df7194
Louise Andrea Tatoy <152823627+AndreaTatoy@users.noreply.github.com>
2024-05-04 18:50:41 +0800

    Rounded Button

---
+++ RoundedButton.java
@@ -0,0 +1,56 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class RoundedButton extends JButton {
+
+    private final int arcWidth;
+    private final int arcHeight;
+    private Color borderColor;
+
+    public RoundedButton(String text, int arcWidth, int arcHeight) {
+        super(text);
+        this.arcWidth = arcWidth;
+        this.arcHeight = arcHeight;
+        setContentAreaFilled(false);
+        setBorderPainted(false);
+        setFocusPainted(false);
+        setBorder(BorderFactory.createEmptyBorder());
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g.create();
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON); 
+
+        if (getModel().isArmed()) {
+            g.setColor(getBackground().darker()); 
+        } else {
+            g.setColor(getBackground());
+        }
+        
+        g.fillRoundRect(0, 0, getWidth(), getHeight(), arcWidth, arcHeight);
+
+        super.paintComponent(g); 
+    }
+
+    public void setBorderColor(Color color) {
+        this.borderColor = color;
+        repaint();
+    }
+
+    @Override
+    protected void paintBorder(Graphics g) {
+        if (borderColor == null) {
+            return;
+        }
+        
+        Graphics2D g2d = (Graphics2D) g.create();
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        g2d.setColor(borderColor); 
+        g2d.setStroke(new BasicStroke(2));
+        g2d.drawRoundRect(0, 0, getWidth() - 1, getHeight() - 1, arcWidth, arcHeight); 
+        
+        g2d.dispose();
+    }
+}

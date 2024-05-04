30e31ba3adcb881cc46f621298ce72f2f59341df
Louise Andrea Tatoy <152823627+AndreaTatoy@users.noreply.github.com>
2024-05-04 18:51:56 +0800

    Rounded Text Field

---
+++ RoundedTextField.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class RoundedTextField extends JTextField {
+
+    private final int arcWidth;
+    private final int arcHeight;
+
+    public RoundedTextField(int columns, int arcWidth, int arcHeight) {
+        super(columns); 
+        this.arcWidth = arcWidth;
+        this.arcHeight = arcHeight;
+        setOpaque(false);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        if (!isOpaque()) {
+            Graphics2D g2d = (Graphics2D) g.create();
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+            g2d.setColor(getBackground());
+            g2d.fillRoundRect(0, 0, getWidth(), getHeight(), arcWidth, arcHeight);
+            g2d.dispose();
+        }
+
+        super.paintComponent(g); 
+    }
+
+    @Override
+    protected void paintBorder(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g.create();
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        g2d.setColor(getForeground()); 
+        g2d.setStroke(new BasicStroke(2));
+        g2d.drawRoundRect(0, 0, getWidth() - 1, getHeight() - 1, arcWidth, arcHeight);
+
+        g2d.dispose();
+    }
+}
